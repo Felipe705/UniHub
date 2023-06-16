e523e227effd2841fac48d6943d87ef95187329d
Felipe705 <delavedova44@gmail.com>
2023-06-16 17:30:49 -0300

    salve

---
+++ src/components/header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class header extends React.Component {
+   render() {
+    return (
+        <div>
+            <header>
+                <a>UNIHUB</a>
+                <nav>
+                    <ul>
+                        <li>HOME</li>
+                        <li>EXPLORAR</li>
+                        <li>CONTATO</li>
+                        <li>SOBRE</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+   }
+}
+
+export default header
